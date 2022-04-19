ec28677f8249c8f3d336e5115731cce7fabd88e1
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-04-19 20:51:51 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import tailwind from "./tailwindcss-icon.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App min-h-screen flex flex-col justify-center items-center bg-[#343a40]">
+      <div className="flex w-[50%] justify-center items-center">
+        <img src={logo} alt="react-logo" className="max-w-[350px] App-logo" />
+        <img src={tailwind} alt="tailwind-logo" className="w-[250px]" />
+      </div>
+      <div className="text-white flex flex-col gap-y-5">
+        <h1 className="font-bold text-5xl">Create-react-app + TailwindCSS</h1>
+        <p className="text-2xl font-semibold">
+          Edit src/App.js and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2 className="hover:text-cyan-500 transition duration-150 ease-linear">
+          <a
+            href="https://reactjs.org/tutorial/tutorial.html"
+            className="underline text-xl"
+            target="_blank"
+          >
+            Learn React
+          </a>
+        </h2>
+      </div>
     </div>
   );
 }
